cb79bb045eebfb92757c172d77d82299c0c10c8d
Yon <yon.handri@bssn.go.id>
2020-01-03 11:01:58 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# CI Admin Starter (CIAS)
+
+### Description
+
+CIAS provide Login Authentication, User Management, Web Setting and User Profile build on CodeIgniter framework(version 3.x). You can add more modules as needed.
+
+# Feature List
+- Built with CodeIgniter 3.x
+- HMVC architecture
+- User registration
+- User Login
+- Login with email or username
+- Password reset
+- Avatar and logo upload
+- E-Mail verification for new users
+- Manage Users
+- Invite new user for registration
+- Restrict Public User Registration
+- Manage User Types
+- Manage permissions
+- Assign permission to User Type
+- Built using AdminLTE
+- Bootstrap Responsive Theme
+- Email Templates for Forgot Password, Invite users
+- SMTP Email Setting
+- Site Logo, Title, Favicon Change settings
+
+Installation
+----
+- Extract Source Code
+- Import cias_db.sql to your database
+- Configure file database.php 
+- Default user: admin and password: admin
+
+Enjoy...
---
+++ application/modules/about/views/index.php
@@ -0,0 +1,30 @@
+<!-- page content -->
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper settingPage">
+    <!-- Main content -->
+    <section class="content">
+      <!-- Default box -->
+      <div class="box box-success">
+        <div class="box-header with-border">
+          <h3 class="box-title">About Us </h3>
+        </div>
+        <div class="box-body">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="col-md-6 col-md-offset-1 aboutshift">
+                <p>This is Code Igniter Admin Starter that build with modular MVC. This containts basic function such as User Management, Web Settings and User Profile. You can add more modules as much as needed by following the pattern </p>
+
+                <p>Visit my github in snahvalabs for more projects.</p>
+                <p>URL: <a href="http://www.github.com/snahvalabs">http://www.github.com/snahvalabs</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
